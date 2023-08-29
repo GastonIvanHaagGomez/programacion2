757acf2622e09478c220e773a661631ac9c88cf9
GastonIvanHaag <gasel88@hotmail.com>
2023-08-29 10:15:39 -0300

    mi porimer cambio

--- participantes.py
+++ participantes.py
@@ -2,4 +2,5 @@ print ("Acá estuvo el profesor Joel Partida. Saludos a todos")
 print ("Esto es un primer cambio hecho por Joel Partido y revisado por Joel Partida")
 print ("Gaston")
 print("Ivan")
+print ("haag")
 
